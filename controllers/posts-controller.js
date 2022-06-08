854f0d188bfbf434d642dcad217124efdeb1f9ed
Marco Busellato <marco.busellato@develondigital.com>
2022-06-08 17:35:23 +0200

    create GET posts

--- controllers/posts-controller.js
+++ controllers/posts-controller.js
@@ -1,18 +1,20 @@
 const Post = require('../models/Post')
 
 const GET_fetchPosts = (req, res, next) => {
-  const test = [{ title: 'Primo' }, { title: 'Secondo' }]
-
-  res.status(200).json(test)
+  Post.find().then(posts => {
+    res.status(200).json(posts)
+  }).catch(error => console.log(error))
 }
 
 const POST_createPost = (req, res, next) => {
+  const user_id = req.user._id
   const { title, image_url, content, author } = req.body
   const created_at = new Date()
   const updated_at = new Date()
   const post = new Post({ title, image_url, content, author, created_at, updated_at })
+
   post.save().then(post => {
-    res.status(200).json({ message: 'post created', post })
+    res.status(201).json({ message: 'post created', post })
   }).catch(error => console.log(error))
 }
 
